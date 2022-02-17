83e9bbafe336d077b3fde9557be96009e48652c7
Catt-21 <cewalsh@uwm.edu>
2022-02-17 16:13:07 -0600

    login page set up

--- Group3Capstone/views.py
+++ Group3Capstone/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import User #create and add user models
+from .classes.administrator import Admin
 # Create your views here.
+
+class Home(View):
+    def get(self, request):
+        request.session.pop("username", None)
+        return render(request, "home.html", {})
+    def post(self, request):
+        noSuchUser = False
+        badPassword = False
+        try:
+            m = User.objects.get(username=request.POST['username'])
+            badPassword = (m.password != request.POST['password'])
+        except:
+            noSuchUser = True
+        if noSuchUser:
+            return render(request, "home.html", {"message":"user does not exist"})
+        elif badPassword:
+            return render(request, "home.html", {"message":"bad password"})
+        else:
+            request.session["username"] = m.username
+            return redirect("/dashboard/")
