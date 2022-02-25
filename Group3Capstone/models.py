0891346314a9982e87f2cc8bc8d3625d0b3ee003
brettclevidence <clevide2@uwm.edu>
2022-02-24 19:09:23 -0600

    I did 'models.model' and not 'models.Model'... and put in the commented out code for adding static file for css, wont work bc I don't have django configuration setup

--- Group3Capstone/models.py
+++ Group3Capstone/models.py
@@ -12,13 +12,13 @@ class User(models.Model):
     type = models.CharField(max_length=1, choices=AccountType.choices, default=AccountType.Administrator)
 
 
-class Sports(models.model):
+class Sports(models.Model):
         name = models.CharField(max_length = 20) #just the name....
 
-class Location(models.model):
+class Location(models.Model):
         location = models.CharField(max_length=30) #just a name
 
-class Events(models.model):
+class Events(models.Model):
         events = models.CharField(max_length = 30) #if want anything else add it
 
 class Admin(User):
