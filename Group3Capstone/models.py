079f239ba242affc999448158eabc4468b9fd91f
brettclevidence <clevide2@uwm.edu>
2022-03-04 11:52:20 -0600

    figured out css..................
    Also models was throwing an error because we had duplicate models, I just commented them out.

--- Group3Capstone/models.py
+++ Group3Capstone/models.py
@@ -52,14 +52,14 @@ class Reservation(models.Model):
 
 
 
-class Sports(models.Model):
-        name = models.CharField(max_length = 20) #just the name....
+#class Sports(models.Model):
+#       name = models.CharField(max_length = 20) #just the name....
 
-class Location(models.Model):
-        location = models.CharField(max_length=30) #just a name
+#class Location(models.Model):
+#       location = models.CharField(max_length=30) #just a name
 
-class Events(models.Model):
-        events = models.CharField(max_length = 30) #if want anything else add it
+#class Events(models.Model):
+#     events = models.CharField(max_length = 30) #if want anything else add it
 
 class Admin(User):
    pass
