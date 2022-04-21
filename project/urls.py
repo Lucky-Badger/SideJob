42dc47c8c04b22c650b8662827af9262bd761e9d
brettClev <clevide2@uwm.edu>
2022-04-21 13:53:49 -0500

    fixed groups so that the button says "join group" and when you click on it, a hidden input will pass the value of the group id so you can join the group
    added an email piece that sends you a notification email every time you join a group. I made a fake gmail, the credentials are in the settings.py, that's why settings.py is changed.
    
    proper cosmetic stuff that never got cleared

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from Group3Capstone import views
 from Group3Capstone.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +27,4 @@ urlpatterns = [
     path('editaccount/', EditAccountPage.as_view()),
     path('createGroupPage/', CreateGroupPage.as_view()),
     path('notSignedIn/', NotSignedIn.as_view()),
-    path('groupEventPage/', GroupEventsPage.as_view()),
-    path('groupEventsPage/<group_id>/', views.GroupEventsPage.as_view(), name="GroupEventsPage"),
 ]
